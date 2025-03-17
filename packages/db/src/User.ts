a9679742c5f4a1971faf19c854261359d70ef3ad
j4mlfy <j4mlfy@gmail.com>
2025-03-17 16:16:54 -0500

    autoupdate 2025-03-17-16:16:54

--- packages/db/src/User.ts
+++ packages/db/src/User.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from './connection';
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
@@ -13,10 +13,59 @@ const UserSchema = new Schema({
       message: props => `${props.value} is not a valid phone number!`
     },
   },
+  hash; {
+  	type: String,
+    required: true,
+  },
   date: Date
 });
+
+class UserClass {
+	setPassword(pass){
+		this.password = password( passwordString, this._id );
+	}
+
+	async hash(password) {
+	    return new Promise((resolve, reject) => {
+	        const salt = crypto.randomBytes(8).toString("hex")
+
+	        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+	            if (err) reject(err);
+	            resolve(salt + ":" + derivedKey.toString('hex'))
+	        });
+	    })
+	}
+
+	async verify(password) {
+	    return new Promise((resolve, reject) => {
+	        const [salt, key] = this.hash.split(":")
+	        crypto.scrypt(password, salt, 64, (err, derivedKey) => {
+	            if (err) reject(err);
+	            resolve(key == derivedKey.toString('hex'))
+	        });
+	    })
+	}
+
+
+	static async login(email, password) {
+		const user = await this.findOne({ email });
+  	 	
+  	 	const isPassword = await user.verify(password);
+
+  	 	if(!isPassword){
+  	 		throw new Error('Is not the password') 
+  	 	}
+
+    	return user;
+  }
+}
+
+userSchema.loadClass(UserClass);
 
 UserSchema.pre('save', function(next) {
 	doc.date = new Date();
 	next();
 });
+
+
+export default  mongoose.model('users', UserSchema);
