6967c98d5ca40464518f9d2b511adb02a3b20319
j4mlfy <j4mlfy@gmail.com>
2025-03-17 14:57:00 -0500

    autoupdate 2025-03-17-14:56:59

--- apps/api/src/routes/auth.ts
+++ apps/api/src/routes/auth.ts
@@ -1,8 +1,28 @@
 import { Route } from "express";
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
-import User from '@another/db/User'
+import Jwt from "passport-jwt";
+import User from '@another/db/User';
 
+var opts = {}
+opts.jwtFromRequest = Jwt.ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'yoursite.net';
+
+passport.use(new Jwt.Strategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
 
 passport.use(new LocalStrategy(function verify(username, password, cb) {
   User.findLogin(username, password)
